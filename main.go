06385852474b769b00ba6fc1936d9c56f1b104af
Sarcha Gurbanow <kurbanovsarca@gmail.com>
2023-10-30 22:20:36 +0500

    go mod, go sum

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	col "gopackages/color"
+	. "gopackages/wordz"
+
+	"github.com/fatih/color"
+)
+
+func main() {
+	col.Greet()
+	color.Red("Hello world")
+	for i := 0; i < 5; i++ {
+		fmt.Println(Hello)
+		fmt.Println(Random())
+
+	}
+}
